8e6f5cab59e4efe04c13ca845b35be97fbffeed1
Vishal <vishalkathirvel0@gmail.com>
2026-08-07 12:34:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+// Select the HTML elements
+const button = document.getElementById("surpriseBtn");
+const card = document.getElementById("card");
+const surprise = document.getElementById("surprise");
+const plant = document.getElementById("plant");
+
+// Wait for the button to be clicked
+button.addEventListener("click", function () {
+
+    // Fade out the card
+    card.classList.add("hide");
+
+    // After the fade animation, hide the card and show the surprise section
+    setTimeout(function () {
+        card.style.display = "none";
+        surprise.classList.remove("hidden");
+    }, 800);
+
+    // Show the seed
+    setTimeout(function () {
+        plant.innerHTML = "🌱";
+    }, 1000);
+
+    // Grow into a small plant
+    setTimeout(function () {
+        plant.innerHTML = "🌿";
+    }, 2500);
+
+    // Grow into a tree
+    setTimeout(function () {
+        plant.innerHTML = "🌳";
+    }, 4500);
+setTimeout(function(){
+
+    plant.innerHTML = `
+        🌳
+        <br><br>
+        <h2>Happiest Birthday MINE 💙</h2>
+
+        <p>
+        May your life bloom like these flowers 🌸<br>
+        Keep smiling, keep shining,<br>
+        and never stop being the amazing person you are.
+        AND YOU ARE MINE FOREVER 💙
+        </p>
+    `;
+
+    for(let i = 0; i < 50; i++){
+
+        setTimeout(createFlower, i*150);
+
+    }
+
+},6500);
+    function createFlower(){
+
+    const flower = document.createElement("div");
+
+    flower.classList.add("flower");
+
+    const flowers = ["🌸","🌺","🌷","🌼","💐","🌹"];
+
+    flower.innerHTML =
+        flowers[Math.floor(Math.random()*flowers.length)];
+
+    flower.style.left = Math.random()*100 + "vw";
+
+    flower.style.animationDuration =
+        (Math.random()*3 + 4) + "s";
+
+    flower.style.fontSize =
+        (Math.random()*20 + 20) + "px";
+
+    document.body.appendChild(flower);
+
+    setTimeout(function(){
+
+        flower.remove();
+
+    },7000);
+
+}
+});
+
+
